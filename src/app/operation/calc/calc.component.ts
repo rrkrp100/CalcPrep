21519f145a10d27b3851cbf6fce09183f3b34419
rrkrp100 <rrkrp100@gmail.com>
2023-12-31 22:44:23 +0530

    gh-pages

---
+++ src/app/operation/calc/calc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  standalone: true,
+  imports: [],
+  templateUrl: './calc.component.html',
+  styleUrl: './calc.component.scss'
+})
+export class CalcComponent {
+
+}
